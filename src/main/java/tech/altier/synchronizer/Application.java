9ce086b9ba29ebd23e3073252b57c30852213d03
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-19 22:44:19 +0530

    Create Application.java

---
+++ src/main/java/tech/altier/synchronizer/Application.java
@@ -0,0 +1,22 @@
+package tech.altier.synchronizer;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-scene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
