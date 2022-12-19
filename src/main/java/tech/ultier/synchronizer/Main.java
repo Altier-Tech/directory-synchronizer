82d309d38b4fafc5752bff345c7f40929a76aa5d
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-19 22:40:31 +0530

    Create Main.java

---
+++ src/main/java/tech/ultier/synchronizer/Main.java
@@ -0,0 +1,14 @@
+package tech.ultier.synchronizer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Main {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
