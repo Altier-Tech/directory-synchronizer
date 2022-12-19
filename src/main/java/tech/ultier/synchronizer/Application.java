10fd0bb1e6b364ebc1030dba5be099f6569798c7
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-19 22:40:28 +0530

    Update Application.java

--- src/main/java/tech/ultier/synchronizer/Application.java
+++ src/main/java/tech/ultier/synchronizer/Application.java
@@ -1,16 +1,15 @@
 package tech.ultier.synchronizer;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
