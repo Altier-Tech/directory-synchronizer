5e7edce03243cf064bb07d9c547dbb0321084650
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 18:03:20 +0530

    Update Main.java

--- src/main/java/tech/altier/synchronizer/Main.java
+++ src/main/java/tech/altier/synchronizer/Main.java
@@ -9,11 +9,10 @@ import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class Main {
     public static LocalRepository repository;
-    private static final String ACCESS_TOKEN;
+    private static String ACCESS_TOKEN;
 
     public static DbxClientV2 client;
     private static String accountName = "ERR!";
@@ -21,6 +20,7 @@ public class Main {
     @FXML
     private ListView<String> listView;
 
+    /*
     static {
         repository = SetupController.repository;
 
@@ -31,15 +31,28 @@ public class Main {
 
         try {
             accountName = client.users().getCurrentAccount().getName().getDisplayName();
-            System.out.println(ThreadColor.ANSI_BLUE + "MainApp: \t" + "Logged in user: " + accountName);
         } catch (DbxException e) {
             System.out.println(ThreadColor.ANSI_RED + "MainApp: \t" + "Error loading user data!");
         }
     }
+    */
 
     public void initialize() {
+        repository = SetupController.repository;
+
+        ACCESS_TOKEN = LoginController.ACCESS_TOKEN;
+
+        DbxRequestConfig config = DbxRequestConfig.newBuilder("Altier").build();
+        client = new DbxClientV2(config, ACCESS_TOKEN);
+
+        try {
+            accountName = client.users().getCurrentAccount().getName().getDisplayName();
+        } catch (DbxException e) {
+            System.out.println(ThreadColor.ANSI_RED + "MainApp: \t" + "Error loading user data!");
+        }
+
         log("Logged in user: " + accountName);
-        log("Local repository: " + repository);
+        log("Local repository: " + repository.getPath());
     }
 
     private void log(String message) {
