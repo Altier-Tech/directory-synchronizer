f95506f208e3086692b36ac32d0f5b3db61561e3
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 09:59:44 +0530

    Update LoginController.java

--- src/main/java/tech/altier/synchronizer/LoginController.java
+++ src/main/java/tech/altier/synchronizer/LoginController.java
@@ -1,10 +1,15 @@
 package tech.altier.synchronizer;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
 
-public class LoginController {
+import java.io.IOException;
+
+public class LoginController extends javafx.application.Application {
     @FXML
     private PasswordField accessTokenField;
     @FXML
@@ -21,5 +26,17 @@ public class LoginController {
         if (ACCESS_TOKEN == null || ACCESS_TOKEN.isEmpty()) {
             label.setText("Please enter a valid access token!");
         }
+
+        // Try loading the main scene
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-scene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("DirSync");
+        stage.setScene(scene);
+        stage.show();
     }
 }
