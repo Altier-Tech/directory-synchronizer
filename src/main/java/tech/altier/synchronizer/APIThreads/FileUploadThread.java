3769e5c8a6216f9bbe1c6a7dd782f6927dde79db
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 17:47:53 +0530

    Update FileUploadThread.java

--- src/main/java/tech/altier/synchronizer/APIThreads/FileUploadThread.java
+++ src/main/java/tech/altier/synchronizer/APIThreads/FileUploadThread.java
@@ -4,9 +4,11 @@ import tech.altier.synchronizer.RemoteHandler.DropboxClient;
 
 public class FileUploadThread implements Runnable {
     private String path;
+    private DropboxClient client;
 
     public FileUploadThread(String path) {
         this.path = path;
+        client = new DropboxClient();
     }
 
     @Override
