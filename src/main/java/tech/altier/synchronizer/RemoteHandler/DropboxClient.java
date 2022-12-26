be4d7204ad11b1b8573ab990f191874c3cb51a2d
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 17:42:58 +0530

    Update DropboxClient.java

--- src/main/java/tech/altier/synchronizer/RemoteHandler/DropboxClient.java
+++ src/main/java/tech/altier/synchronizer/RemoteHandler/DropboxClient.java
@@ -1,4 +1,12 @@
 package tech.altier.synchronizer.RemoteHandler;
 
+import com.dropbox.core.v2.DbxClientV2;
+import tech.altier.synchronizer.Main;
+
 public class DropboxClient {
+    public static DbxClientV2 client;
+
+    public DropboxClient () {
+        client = Main.client;
+    }
 }
