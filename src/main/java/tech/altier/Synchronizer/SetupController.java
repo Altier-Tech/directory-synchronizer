37015f302c4bd3162dfaefb107b3c7431ddf7de8
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 16:46:21 +0530

    Update SetupController.java

--- src/main/java/tech/altier/Synchronizer/SetupController.java
+++ src/main/java/tech/altier/Synchronizer/SetupController.java
@@ -1,5 +1,12 @@
 package tech.altier.Synchronizer;
 
+import javafx.fxml.FXML;
+
 public class SetupController {
-    
+
+
+    @FXML
+    public void onChooseFolderButtonClick() {
+        
+    }
 }
