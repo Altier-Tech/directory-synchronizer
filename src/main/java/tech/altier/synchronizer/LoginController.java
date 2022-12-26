d7a987c5cc48fc41465decb9054f8a1ce4785419
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 10:28:35 +0530

    Working - switching scenes

--- src/main/java/tech/altier/synchronizer/LoginController.java
+++ src/main/java/tech/altier/synchronizer/LoginController.java
@@ -1,9 +1,6 @@
 package tech.altier.synchronizer;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 
