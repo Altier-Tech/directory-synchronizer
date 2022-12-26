75ed72754f77385f2212a3ca63c162200c30aa56
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 09:49:41 +0530

    Update LoginController.java

--- src/main/java/tech/altier/synchronizer/LoginController.java
+++ src/main/java/tech/altier/synchronizer/LoginController.java
@@ -1,14 +1,17 @@
 package tech.altier.synchronizer;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 
 public class LoginController {
     @FXML
+    private PasswordField passwordField;
 
     public static String ACCESS_TOKEN;
 
     @FXML
     private void onLoginButtonClick() {
-        System.out.println("Login button clicked");
+        System.out.println("Login button clicked!");
+        ACCESS_TOKEN = passwordField.getText();
     }
 }
