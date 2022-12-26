f634ccf04566edd602c4bbc9a4fa0de91b7c7c38
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 16:46:47 +0530

    Update SetupController.java

--- src/main/java/tech/altier/Synchronizer/SetupController.java
+++ src/main/java/tech/altier/Synchronizer/SetupController.java
@@ -1,12 +1,17 @@
 package tech.altier.Synchronizer;
 
 import javafx.fxml.FXML;
+import tech.altier.Thread.ThreadColor;
 
 public class SetupController {
 
 
     @FXML
     public void onChooseFolderButtonClick() {
-        
+
+    }
+
+    private void log(String message) {
+        System.out.println(ThreadColor.ANSI_PURPLE + "SetupView: \t" + message);
     }
 }
