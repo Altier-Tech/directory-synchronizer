90b9b7ac2fded4fc310e62b8b0dd674753cf4ea5
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 17:27:57 +0530

    Update LocalRepository.java

--- src/main/java/tech/altier/synchronizer/LocalHandler/LocalRepository.java
+++ src/main/java/tech/altier/synchronizer/LocalHandler/LocalRepository.java
@@ -3,21 +3,9 @@ package tech.altier.synchronizer.LocalHandler;
 import java.io.File;
 
 public class LocalRepository {
-    private String absPath;
+    private String path;
 
-    public LocalRepository(String absPath) {
-        this.absPath = absPath;
-    }
-
-    // List all files in the directory
-    public void listFiles() {
-        File folder = new File(absPath);
-        File[] listOfFiles = folder.listFiles();
-
-        for (File file : listOfFiles) {
-            if (file.isFile()) {
-                System.out.println(file.getName());
-            }
-        }
+    public LocalRepository(String path) {
+        this.path = path;
     }
 }
