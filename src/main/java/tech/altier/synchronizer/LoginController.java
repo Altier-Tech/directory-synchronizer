0ee286c0994379508af76bd792cf918345944ba3
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 13:51:06 +0530

    Update LoginController.java

--- src/main/java/tech/altier/synchronizer/LoginController.java
+++ src/main/java/tech/altier/synchronizer/LoginController.java
@@ -1,6 +1,7 @@
 package tech.altier.synchronizer;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 
@@ -8,11 +9,17 @@ import java.io.IOException;
 
 public class LoginController {
     @FXML
+    public Hyperlink generateTokenLink;
+    @FXML
     private PasswordField accessTokenField;
     @FXML
     private Label label;
 
     public static String ACCESS_TOKEN;
+    
+    public void initialize() {
+        generateTokenLink.setText("https://www.dropbox.com/oauth2/authorize?client_id=znj7h8gmdmevoz0&token_access_type=offline&response_type=code");
+    }
 
     @FXML
     private void onResetButtonClick() {
