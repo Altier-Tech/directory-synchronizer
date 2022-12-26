41589371918dabcd647dfa5da954fa0fa2501cb0
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 17:35:49 +0530

    Create FileUploadThread.java

---
+++ src/main/java/tech/altier/synchronizer/LocalHandler/FileUploadThread.java
@@ -0,0 +1,14 @@
+package tech.altier.synchronizer.LocalHandler;
+
+public class FileUploadThread extends Runnable {
+    private String path;
+
+    public FileUploadThread(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public void run() {
+        // TODO
+    }
+}
