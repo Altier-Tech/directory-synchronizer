d67d38a754b0746ea275bb9f66da61ce22bdbd9a
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 17:37:16 +0530

    Update FileHandler.java

--- src/main/java/tech/altier/synchronizer/LocalHandler/FileHandler.java
+++ src/main/java/tech/altier/synchronizer/LocalHandler/FileHandler.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 
 public class FileHandler {
     public static void handleLocalCreate(Path filePath) { // TODO
-        //
+        // TODO kick off a FileUploadThread with the path
     }
 
     public static void handleLocalDelete(Path filePath) { // TODO
