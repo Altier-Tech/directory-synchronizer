b443fc3721f54710b4c0e56cbf95417f2131ff7a
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 10:16:35 +0530

    Update Application.java

--- src/main/java/tech/altier/synchronizer/Application.java
+++ src/main/java/tech/altier/synchronizer/Application.java
@@ -7,19 +7,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Application extends javafx.application.Application {
     private Stage primaryStage;
 
     @Override
     public void start(Stage stage) throws IOException {
-        this.primaryStage = stage;
-
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("DirSync");
         stage.setScene(scene);
         stage.show();
+        
+        this.primaryStage = stage;
     }
 
     public static void main(String[] args) throws DbxException, IOException {
@@ -27,8 +28,7 @@ public class Application extends javafx.application.Application {
     }
 
     public void loadMainStage() throws IOException {
-        Parent pane = FXMLLoader.load(getClass().getResource("main-scene.fxml"));
-
+        Parent pane = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("main-scene.fxml")));
         primaryStage.getScene().setRoot(pane);
     }
 }
