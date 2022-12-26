16454da5da1e325ae49474da71fd754a05f50e62
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 16:48:21 +0530

    Update SetupController.java

--- src/main/java/tech/altier/Synchronizer/SetupController.java
+++ src/main/java/tech/altier/Synchronizer/SetupController.java
@@ -1,14 +1,22 @@
 package tech.altier.Synchronizer;
 
 import javafx.fxml.FXML;
+import javafx.stage.DirectoryChooser;
 import tech.altier.Thread.ThreadColor;
 
+import java.io.File;
+
 public class SetupController {
-
-
     @FXML
     public void onChooseFolderButtonClick() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File selectedDirectory = directoryChooser.showDialog(Application.primaryStage);
 
+        if (selectedDirectory == null) {
+            //No Directory selected
+        } else {
+            log(selectedDirectory.getAbsolutePath());
+        }
     }
 
     private void log(String message) {
