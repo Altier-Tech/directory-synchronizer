30e7004fdbf521ebae5ef3cf805a7076cf0e5b4e
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 10:05:08 +0530

    Update Application.java

--- src/main/java/tech/altier/synchronizer/Application.java
+++ src/main/java/tech/altier/synchronizer/Application.java
@@ -2,12 +2,15 @@ package tech.altier.synchronizer;
 
 import com.dropbox.core.DbxException;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+    private Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-scene.fxml"));
@@ -22,6 +25,9 @@ public class Application extends javafx.application.Application {
     }
 
     public static void switchToMainStage() {
-        
+        Parent pane = FXMLLoader.load(
+                getClass().getResource(fxml));
+
+        primaryStage.getScene().setRoot(pane);
     }
 }
