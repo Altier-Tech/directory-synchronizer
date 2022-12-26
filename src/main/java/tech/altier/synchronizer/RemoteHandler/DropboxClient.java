ca403c77107227d9f73edfa978455a8890bfaa9d
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 17:46:04 +0530

    Update DropboxClient.java

--- src/main/java/tech/altier/synchronizer/RemoteHandler/DropboxClient.java
+++ src/main/java/tech/altier/synchronizer/RemoteHandler/DropboxClient.java
@@ -4,6 +4,7 @@ import com.dropbox.core.DbxException;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.FileMetadata;
 import com.dropbox.core.v2.files.UploadErrorException;
+import tech.altier.Thread.ThreadColor;
 import tech.altier.synchronizer.Main;
 
 import java.io.FileInputStream;
@@ -19,10 +20,15 @@ public class DropboxClient {
 
     public static void uploadFile(String path) {
         try (InputStream in = new FileInputStream(path)) {
+            log("File upload started: " + path);
             FileMetadata metadata = client.files().uploadBuilder(path)
                     .uploadAndFinish(in);
         } catch (IOException | DbxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static void log(String message) {
+        System.out.println(ThreadColor.ANSI_CYAN + "DBClient: \t" + message);
     }
 }
