5df327551ab7c25482bc6703e6357288491138a1
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 17:18:28 +0530

    Update LocalRepository.java

--- src/main/java/tech/altier/synchronizer/FileHandler/LocalRepository.java
+++ src/main/java/tech/altier/synchronizer/FileHandler/LocalRepository.java
@@ -1,4 +1,11 @@
 package tech.altier.synchronizer.FileHandler;
 
 public class LocalRepository {
+    private String absPath;
+
+    public LocalRepository(String absPath) {
+        this.absPath = absPath;
+    }
+
+    // List all files in the directory
 }
