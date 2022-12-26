e735fc11ae18f1e4902908eac6327fe179879cf3
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 09:48:44 +0530

    Update LoginController.java

--- src/main/java/tech/altier/synchronizer/LoginController.java
+++ src/main/java/tech/altier/synchronizer/LoginController.java
@@ -3,7 +3,9 @@ package tech.altier.synchronizer;
 import javafx.fxml.FXML;
 
 public class LoginController {
+    @FXML
 
+    public static String ACCESS_TOKEN;
 
     @FXML
     private void onLoginButtonClick() {
