c0ac6f52ad469d4320b52025641831e2204e76e8
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 09:38:52 +0530

    Update Application.java

--- src/main/java/tech/altier/synchronizer/Application.java
+++ src/main/java/tech/altier/synchronizer/Application.java
@@ -18,7 +18,6 @@ public class Application extends javafx.application.Application {
     }
 
     public static void main(String[] args) throws DbxException, IOException {
-
         launch();
     }
 }
