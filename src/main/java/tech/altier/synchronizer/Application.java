3bb10c2b9ab043fa6a166619f3126ab67faf01fd
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 09:53:01 +0530

    Update Application.java

--- src/main/java/tech/altier/synchronizer/Application.java
+++ src/main/java/tech/altier/synchronizer/Application.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-scene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("DirSync");
         stage.setScene(scene);
