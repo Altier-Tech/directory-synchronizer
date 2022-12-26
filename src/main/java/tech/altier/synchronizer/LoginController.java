eb425b94591de3eb81b5c7e1be7f02f2d50d2d80
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 14:20:06 +0530

    Update LoginController.java

--- src/main/java/tech/altier/synchronizer/LoginController.java
+++ src/main/java/tech/altier/synchronizer/LoginController.java
@@ -1,13 +1,9 @@
 package tech.altier.synchronizer;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
 
 import java.awt.*;
 import java.io.IOException;
