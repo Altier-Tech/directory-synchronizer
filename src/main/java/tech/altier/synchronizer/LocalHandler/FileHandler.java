33b2e51ae0089dc75dd10435d4c5b057f8f2c386
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-26 17:34:32 +0530

    Update FileHandler.java

--- src/main/java/tech/altier/synchronizer/LocalHandler/FileHandler.java
+++ src/main/java/tech/altier/synchronizer/LocalHandler/FileHandler.java
@@ -1,4 +1,14 @@
 package tech.altier.synchronizer.LocalHandler;
 
+import java.nio.file.Path;
+
 public class FileHandler {
+    public static void handleLocalCreate(Path filePath) { // TODO
+    }
+
+    public static void handleLocalDelete(Path filePath) { // TODO
+    }
+
+    public static void handleLocalModify(Path filePath) { // TODO
+    }
 }
