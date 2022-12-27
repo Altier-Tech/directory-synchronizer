6cba4b840055179573e7612871369df4d2ec139f
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 09:44:01 +0530

    Update FileHandler.java

--- src/main/java/tech/altier/synchronizer/LocalHandler/FileHandler.java
+++ src/main/java/tech/altier/synchronizer/LocalHandler/FileHandler.java
@@ -10,7 +10,7 @@ import static tech.altier.synchronizer.Main.repository;
 public class FileHandler {
     public void handleLocalCreate(Path filePath) {
         // TODO kick off a FileUploadThread with the path
-        log("Upload thread started for new created file...);
+        log("Upload thread started for new created file...");
         Thread uploadThread = new Thread(
                 new FileUploadThread(repository.getPath() + "\\" + filePath)
         );
