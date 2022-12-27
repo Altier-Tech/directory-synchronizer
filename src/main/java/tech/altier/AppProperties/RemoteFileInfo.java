f43ca85cf7a2c233449f3ba319e075179ad2553b
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 17:36:23 +0530

    Update RemoteFileInfo.java

--- src/main/java/tech/altier/AppProperties/RemoteFileInfo.java
+++ src/main/java/tech/altier/AppProperties/RemoteFileInfo.java
@@ -1,4 +1,34 @@
 package tech.altier.AppProperties;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Properties;
+
 public class RemoteFileInfo {
+    static Properties conf;
+    public static HashMap<String, String> remoteFiles;
+
+    static {
+        try {
+            conf = loadProperties();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Properties loadProperties() throws IOException {
+        Properties configuration = new Properties();
+        InputStream inputStream = PropertiesLoader.class
+                .getClassLoader()
+                .getResourceAsStream("application.properties");
+        configuration.load(inputStream);
+        assert inputStream != null;
+        inputStream.close();
+        return configuration;
+    }
+
+    public static String get(String key) {
+        return conf.getProperty(key);
+    }
 }
