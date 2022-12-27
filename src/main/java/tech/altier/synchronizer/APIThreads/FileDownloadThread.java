1e04c43adbf0c9cdb0ac3997ee48287ff9176803
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 10:37:58 +0530

    Update FileDownloadThread.java

--- src/main/java/tech/altier/synchronizer/APIThreads/FileDownloadThread.java
+++ src/main/java/tech/altier/synchronizer/APIThreads/FileDownloadThread.java
@@ -1,10 +1,17 @@
 package tech.altier.synchronizer.APIThreads;
 
 import tech.altier.synchronizer.RemoteHandler.DropboxClient;
+import com.dropbox.core.DbxException;
+import tech.altier.Thread.ThreadColor;
+import tech.altier.synchronizer.RemoteHandler.DropboxClient;
 
 public class FileDownloadThread {
     private final String path;
     private final DropboxClient client;
 
-    
+    public FileDownloadThread(String path) {
+        log("Upload thread initialized for file " + path);
+        this.path = path;
+        client = new DropboxClient();
+    }
 }
