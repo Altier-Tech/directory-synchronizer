0d21fc660afecb25f9916f0ef25268083630a405
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 18:03:09 +0530

    Update RemoteListener.java

--- src/main/java/tech/altier/synchronizer/RemoteHandler/RemoteListener.java
+++ src/main/java/tech/altier/synchronizer/RemoteHandler/RemoteListener.java
@@ -3,6 +3,13 @@ package tech.altier.synchronizer.RemoteHandler;
 public class RemoteListener implements Runnable {
     @Override
     public void run() {
+        // Adding a waiting period of 10 seconds
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
+        
     }
 }
