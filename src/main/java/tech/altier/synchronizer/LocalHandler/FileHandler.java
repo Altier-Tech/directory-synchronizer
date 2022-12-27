b4490fc5d1f0f7b6d43df175cdc0f004e0326b9c
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 09:43:36 +0530

    Update FileHandler.java

--- src/main/java/tech/altier/synchronizer/LocalHandler/FileHandler.java
+++ src/main/java/tech/altier/synchronizer/LocalHandler/FileHandler.java
@@ -1,15 +1,29 @@
 package tech.altier.synchronizer.LocalHandler;
 
+import tech.altier.Thread.ThreadColor;
+import tech.altier.synchronizer.APIThreads.FileUploadThread;
+
 import java.nio.file.Path;
 
+import static tech.altier.synchronizer.Main.repository;
+
 public class FileHandler {
-    public static void handleLocalCreate(Path filePath) { // TODO
+    public void handleLocalCreate(Path filePath) {
         // TODO kick off a FileUploadThread with the path
+        log("Upload thread started for new created file...);
+        Thread uploadThread = new Thread(
+                new FileUploadThread(repository.getPath() + "\\" + filePath)
+        );
+        uploadThread.start();
     }
 
     public static void handleLocalDelete(Path filePath) { // TODO
     }
 
     public static void handleLocalModify(Path filePath) { // TODO
+    }
+
+    private void log(String message) {
+        System.out.println(ThreadColor.ANSI_CYAN + "FileHandler: \t" + message);
     }
 }
