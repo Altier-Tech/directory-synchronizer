5f888d228f099635b387933ded336ef387f325a6
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 10:11:13 +0530

    Update ListViewRefreshService.java

--- src/main/java/tech/altier/synchronizer/GUIClient/ListViewRefreshService.java
+++ src/main/java/tech/altier/synchronizer/GUIClient/ListViewRefreshService.java
@@ -1,7 +1,21 @@
 package tech.altier.synchronizer.GUIClient;
 
+import javafx.application.Platform;
 import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
 
 public class ListViewRefreshService extends ScheduledService<Void> {
-    
+    @Override
+    protected Task<Void> createTask(){
+        return new Task<Void>(){
+            @Override
+            protected Void call(){
+                Platform.runLater(() -> {
+                    /* Modify you GUI properties... */
+
+                });
+                return null;
+            }
+        }
+    }
 }
