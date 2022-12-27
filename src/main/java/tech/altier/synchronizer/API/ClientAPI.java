1c48fa79894fe31f337ee7e412b303c278de3f8b
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 16:14:08 +0530

    Update ClientAPI.java

--- src/main/java/tech/altier/synchronizer/API/ClientAPI.java
+++ src/main/java/tech/altier/synchronizer/API/ClientAPI.java
@@ -1,4 +1,14 @@
 package tech.altier.synchronizer.API;
 
+import com.dropbox.core.DbxException;
+import tech.altier.synchronizer.Main;
+
+import java.io.*;
+
 public interface ClientAPI {
+    public void uploadFile(String path) throws DbxException;
+
+    public void downloadFile(String remotePath) throws IOException, DbxException;
+
+    public void deleteFile(String path) throws DbxException;
 }
