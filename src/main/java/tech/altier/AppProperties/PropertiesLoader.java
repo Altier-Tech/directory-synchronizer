dece8d6d1e0e510f39d6b2b5df47c14fbd916b02
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 16:48:19 +0530

    Update PropertiesLoader.java

--- src/main/java/tech/altier/AppProperties/PropertiesLoader.java
+++ src/main/java/tech/altier/AppProperties/PropertiesLoader.java
@@ -1,4 +1,37 @@
 package tech.altier.AppProperties;
 
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
 public class PropertiesLoader {
+
+    private static PropertiesLoader instance;
+    private FileBasedConfiguration configuration;
+
+    private PropertiesLoader() {
+        Parameters params = new Parameters();
+        FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+                new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                        .configure(params.properties()
+                                .setFileName("application.properties"));
+        try {
+            configuration = builder.getConfiguration();
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized PropertiesLoader getInstance() {
+        if (instance == null) {
+            instance = new PropertiesLoader();
+        }
+        return instance;
+    }
+
+    public String getProperty(String key) {
+        return (String) configuration.getProperty(key);
+    }
 }
