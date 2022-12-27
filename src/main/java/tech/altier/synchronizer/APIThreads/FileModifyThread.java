2043e474b27289d9a2807ce4be19d834547eddfc
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 12:15:48 +0530

    Update FileModifyThread.java

--- src/main/java/tech/altier/synchronizer/APIThreads/FileModifyThread.java
+++ src/main/java/tech/altier/synchronizer/APIThreads/FileModifyThread.java
@@ -6,4 +6,30 @@ import tech.altier.Thread.ThreadColor;
 import tech.altier.synchronizer.RemoteHandler.DropboxClient;
 
 public class FileModifyThread implements Runnable {
+    private final String path;
+    private final DropboxClient client;
+
+    public FileDeleteThread(String path) {
+        log("Deletion thread initialized for file " + path);
+        this.path = path;
+        client = new DropboxClient();
+    }
+
+    @Override
+    public void run() {
+        try {
+            client.deleteFile(path);
+        } catch (DbxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void log(String message) {
+        System.out.println(
+                ThreadColor.ANSI_BLUE +
+                Thread.currentThread().getName() +
+                "\tDBClient: \t" +
+                message
+        );
+    }
 }
