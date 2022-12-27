82a15ef7eb147a4821a9ad2e86844fe1436477b2
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 17:43:57 +0530

    Update RemoteFiles.java

--- src/main/java/tech/altier/synchronizer/RemoteHandler/RemoteFiles.java
+++ src/main/java/tech/altier/synchronizer/RemoteHandler/RemoteFiles.java
@@ -1,5 +1,6 @@
 package tech.altier.synchronizer.RemoteHandler;
 
+import com.dropbox.core.DbxException;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.ListFolderResult;
 import com.dropbox.core.v2.files.Metadata;
@@ -8,13 +9,13 @@ import tech.altier.synchronizer.Main;
 public class RemoteFiles {
     private DbxClientV2 client;
 
-    public RemoteFiles() {
+    public RemoteFiles() throws DbxException {
         client = Main.client;
-        
+
         ListFolderResult result = client.files().listFolder("");
         while (true) {
             for (Metadata metadata : result.getEntries()) {
-                listViewRemote.getItems().add(metadata.getName());
+
             }
 
             if (!result.getHasMore()) {
