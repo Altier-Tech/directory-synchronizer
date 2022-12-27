9ed34757fe298cc3f8ebc7b010d93e0c4054a6f2
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 10:37:41 +0530

    Update FileDownloadThread.java

--- src/main/java/tech/altier/synchronizer/APIThreads/FileDownloadThread.java
+++ src/main/java/tech/altier/synchronizer/APIThreads/FileDownloadThread.java
@@ -1,4 +1,10 @@
 package tech.altier.synchronizer.APIThreads;
 
+import tech.altier.synchronizer.RemoteHandler.DropboxClient;
+
 public class FileDownloadThread {
+    private final String path;
+    private final DropboxClient client;
+
+    
 }
