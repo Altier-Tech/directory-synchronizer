b57fe33ee3667c9d0fd93a75a7e654787a33cfa8
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 16:07:55 +0530

    Update RemoteWatcher.java

--- src/main/java/tech/altier/synchronizer/RemoteHandler/RemoteWatcher.java
+++ src/main/java/tech/altier/synchronizer/RemoteHandler/RemoteWatcher.java
@@ -1,4 +1,17 @@
 package tech.altier.synchronizer.RemoteHandler;
 
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.DbxClientV2;
+import tech.altier.synchronizer.Main;
+
 public class RemoteWatcher {
+    public static DbxClientV2 client;
+
+    public RemoteWatcher() {
+        client = Main.client;
+    }
+
+    public void start() throws DbxException {
+
+    }
 }
