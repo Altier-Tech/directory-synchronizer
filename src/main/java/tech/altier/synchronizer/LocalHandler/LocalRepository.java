a3909107dc32101816ab213be45661132f8593f6
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 09:32:38 +0530

    Update LocalRepository.java

--- src/main/java/tech/altier/synchronizer/LocalHandler/LocalRepository.java
+++ src/main/java/tech/altier/synchronizer/LocalHandler/LocalRepository.java
@@ -1,15 +1,9 @@
 package tech.altier.synchronizer.LocalHandler;
 
 import tech.altier.Thread.ThreadColor;
-import tech.altier.synchronizer.Main;
 
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.StandardWatchEventKinds;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
+import java.nio.file.*;
 
 public class LocalRepository {
     private String path;
