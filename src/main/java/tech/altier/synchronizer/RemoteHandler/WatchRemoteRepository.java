2ae3bc4d00a57f938f3dd736eecf4f42d232302e
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 18:05:49 +0530

    Update WatchRemoteRepository.java

--- src/main/java/tech/altier/synchronizer/RemoteHandler/WatchRemoteRepository.java
+++ src/main/java/tech/altier/synchronizer/RemoteHandler/WatchRemoteRepository.java
@@ -1,8 +1,5 @@
 package tech.altier.synchronizer.RemoteHandler;
 
-import com.dropbox.core.v2.DbxClientV2;
-import tech.altier.synchronizer.Main;
-
 public class WatchRemoteRepository {
     public WatchRemoteRepository() {
 
