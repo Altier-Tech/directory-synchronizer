c5f0ff2c2229f098be2d17a1ac32c6bc2b751632
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-27 18:16:56 +0530

    Update RemoteListener.java

--- src/main/java/tech/altier/synchronizer/RemoteHandler/RemoteListener.java
+++ src/main/java/tech/altier/synchronizer/RemoteHandler/RemoteListener.java
@@ -1,13 +1,19 @@
 package tech.altier.synchronizer.RemoteHandler;
 
 import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
 import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
+import tech.altier.synchronizer.Main;
 
 public class RemoteListener implements Runnable {
     static RemoteFiles remoteFilesInstance;
     private ListFolderResult remoteFileListResult;
+    DbxClientV2 client;
 
-    static {
+    public RemoteListener() {
+        client = Main.client;
         remoteFilesInstance = RemoteFiles.getInstance();
     }
 
@@ -21,11 +27,28 @@ public class RemoteListener implements Runnable {
         }
 
         try {
-            ListFolderResult result = remoteFilesInstance.getDetailedRemoteFileInfo();
+            remoteFileListResult = remoteFilesInstance.getDetailedRemoteFileInfo();
         } catch (DbxException e) {
             throw new RuntimeException(e);
         }
 
-        for (String key : result)
+        while (true) {
+            for (Metadata metadata : remoteFileListResult.getEntries()) {
+                FileMetadata fileMetadata = null;
+                if (metadata instanceof FileMetadata) {
+                    fileMetadata = (FileMetadata) metadata;
+                }
+
+                assert fileMetadata != null;
+
+                // TODO
+            }
+
+            if (!remoteFileListResult.getHasMore()) {
+                break;
+            }
+
+            result = client.files().listFolderContinue(remoteFileListResult.getCursor());
+        }
     }
 }
