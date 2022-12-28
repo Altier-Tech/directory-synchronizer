e1312c00255e40a95ba94c2e161fc85d69220845
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-28 18:32:43 +0530

    Update LoginController.java

--- src/main/java/tech/altier/synchronizer/LoginController.java
+++ src/main/java/tech/altier/synchronizer/LoginController.java
@@ -67,7 +67,7 @@ public class LoginController {
 
     @FXML
     public void handleExit(MouseEvent mouseEvent) {
-        // TODO Call the exit method on the main app
+        Application.exit();
     }
 
     @FXML
