fb17ab62d5ab53242760fe1d18ad8520c5e336b6
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-28 18:35:05 +0530

    Update LoginController.java

--- src/main/java/tech/altier/synchronizer/LoginController.java
+++ src/main/java/tech/altier/synchronizer/LoginController.java
@@ -22,7 +22,7 @@ public class LoginController {
     @FXML
     private Label label;
 
-    private static String ABOUT_URL;
+    private static String ABOUT_URL = "";
     public static String ACCESS_TOKEN;
     public static String loginURL;
 
