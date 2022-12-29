bdfcfdcf11e9fd9736c1051cde8b3789b9f5d5a3
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-29 10:44:22 +0530

    Update Auth.java

--- src/main/java/tech/altier/synchronizer/Auth.java
+++ src/main/java/tech/altier/synchronizer/Auth.java
@@ -20,8 +20,6 @@ public class Auth {
         DbxRequestConfig config = DbxRequestConfig.newBuilder("Altier").build();
         client = new DbxClientV2(config, accessToken);
 
-        log(accessToken);
-
         try {
             String accountName = client.users().getCurrentAccount().getName().getDisplayName();
             log("Authorization successful for user " + accountName + "!");
