cabde419b60b29bf3c003e1bded082afd54eabba
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-29 11:35:48 +0530

    Update Main.java

--- src/main/java/tech/altier/synchronizer/Main.java
+++ src/main/java/tech/altier/synchronizer/Main.java
@@ -62,7 +62,7 @@ public class Main {
         repository.startListening();
 
         // Start listening for remote changes
-        
+
 
         // Start service for refreshing the list view in GUI
         startListViewRefreshService();
